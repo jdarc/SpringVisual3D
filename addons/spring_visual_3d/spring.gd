0ca87e1b7d712da6c6b649aaea819bc19e787430
Jean d'Arc <darc.zynaps@gmail.com>
2026-07-25 16:24:54 +0100

    Major refactor of addon

--- addons/spring_visual_3d/spring.gd
+++ addons/spring_visual_3d/spring.gd
@@ -3,163 +3,165 @@
 @tool
 class_name SpringVisual3D extends MeshInstance3D
 
+const SHADER = preload("uid://1r64xr0q0xy3")
+
 @export var anchor_node: Node3D:
-	set(value): anchor_node = value; _update_spring()
-
+	set(value): anchor_node = value; _geometry_state_dirty = true
 @export var target_node : Node3D:
-	set(value): target_node = value; _update_spring()
-
+	set(value): target_node = value; _geometry_state_dirty = true
+@export_range(0.0, 1.0, 0.01) var offset: float:
+	set(value): offset = value; _geometry_state_dirty = true; _shader_state_dirty = true
 @export_range(0.0, 360.0, 0.01) var twist_degrees: float:
-	set(value): twist_degrees = value; _handle_twist_changed()
+	set(value): twist_degrees = value; _geometry_state_dirty = true;
 
 @export_group(&"Geometry")
 @export_range(4, 64, 1, &"or_greater") var radial_segments: int = 12:
-	set(value): radial_segments = value; _update_shader()
+	set(value): radial_segments = value; _geometry_state_dirty = true; _shader_state_dirty = true
 @export_range(0, 512, 1, &"or_greater") var rings: int = 200:
-	set(value): rings = value; _update_shader()
+	set(value): rings = value; _geometry_state_dirty = true; _shader_state_dirty = true
 @export_range(0.0001, 1.0, 0.0001, &"or_greater") var wire_radius: float = 0.05:
-	set(value): wire_radius = value; _update_shader()
+	set(value): wire_radius = value; _geometry_state_dirty = true; _shader_state_dirty = true
 @export_range(0.0001, 2.0, 0.0001, &"or_greater") var coil_radius: float = 0.3:
-	set(value): coil_radius = value; _update_shader()
+	set(value): coil_radius = value; _geometry_state_dirty = true; _shader_state_dirty = true
 @export_range(1, 50, 1, &"prefer_slider") var total_coils: int = 10:
-	set(value): total_coils = value; _update_shader()
+	set(value): total_coils = value; _geometry_state_dirty = true; _shader_state_dirty = true
 
 @export_group(&"Appearance")
 @export var albedo: Color = Color.WHITE:
-	set(value): albedo = value; _update_shader()
-@export_range(0.0, 1.0, 0.01) var metallic: float = 0.8:
-	set(value): metallic = value; _update_shader()
+	set(value): albedo = value; _shader_state_dirty = true
+@export_range(0.0, 1.0, 0.01) var metallic: float = 0.9:
+	set(value): metallic = value; _shader_state_dirty = true
 @export_range(0.0, 1.0, 0.01) var specular: float = 0.5:
-	set(value): specular = value; _update_shader()
+	set(value): specular = value; _shader_state_dirty = true
 @export_range(0.0, 1.0, 0.01) var roughness: float = 0.2:
-	set(value): roughness = value; _update_shader()
+	set(value): roughness = value; _shader_state_dirty = true
 
-static var _cached_shader: Shader
-static var _cached_tube_shader: Shader
-static var _cached_tube_material: ShaderMaterial
+@export_group(&"")
+@export_tool_button(&"Align Control Nodes") var _align_action = _align_handles
 
-var current_length: float:
-	set(value): current_length = maxf(0.0001, value); _update_shader()
-
-var _mesh_container: Node3D
-var _debug_box_instance: MeshInstance3D
-var _spring_mesh_instance: MeshInstance3D
-var _shader_material: ShaderMaterial
-var _current_up_axis: Vector3 = Vector3.UP
-var _target_last_position := Vector3.ZERO
-var _origin_last_position := Vector3.ZERO
-var _cylinder_maker := SpringVisual3DCylinder.new()
-
-
-func _init() -> void:
-	if not _cached_tube_shader:
-		_cached_tube_shader = load(path_to(get_script(), &"tube.gdshader"))
-		_cached_tube_material = ShaderMaterial.new()
-		_cached_tube_material.shader = _cached_tube_shader
-
-	if not _cached_shader:
-		_cached_shader = load(path_to(get_script(), &"spring.gdshader"))
-
-	_shader_material = ShaderMaterial.new()
-	_shader_material.resource_local_to_scene = true
-	_shader_material.shader = _cached_shader
-
-	var cylinder_mesh := CylinderMesh.new()
-	cylinder_mesh.height = 0.001
-	cylinder_mesh.top_radius = 0.001
-	cylinder_mesh.bottom_radius = 0.001
-	_spring_mesh_instance = MeshInstance3D.new()
-	_spring_mesh_instance.rotate_x(PI * 0.5)
-	_spring_mesh_instance.mesh = cylinder_mesh
-	_spring_mesh_instance.set_surface_override_material(0, _shader_material)
-	_spring_mesh_instance.gi_mode = GeometryInstance3D.GI_MODE_DYNAMIC
-
-	_mesh_container = Node3D.new()
-	_mesh_container.add_child(_spring_mesh_instance)
-	add_child(_mesh_container)
-
-	if Engine.is_editor_hint():
-		mesh = _cylinder_maker.generate()
-		set_surface_override_material(0, _cached_tube_material)
-	else:
-		mesh = null
-
+var _current_length := 1.0
+var _current_up_axis := Vector3.UP
+var _geometry_state_dirty := true
+var _shader_state_dirty := true
+var _previous_anchor_origin := Vector3.INF
+var _previous_target_origin := Vector3.INF
 
 func _ready() -> void:
-	current_length = 1.0
-	_handle_twist_changed()
-	_update_spring()
+	_on_mesh_resource_changed(mesh)
 
 
 func _process(delta: float) -> void:
-	_update_spring()
+	if anchor_node and anchor_node.global_position != _previous_anchor_origin:
+		_previous_anchor_origin = anchor_node.global_position
+		_geometry_state_dirty = true
+		_shader_state_dirty = true
+
+	if target_node and target_node.global_position != _previous_target_origin:
+		_previous_target_origin = target_node.global_position
+		_geometry_state_dirty = true
+		_shader_state_dirty = true
+
+	if _geometry_state_dirty:
+		_update_geometry()
+
+	if _shader_state_dirty:
+		_update_shader()
 
 
-func _handle_twist_changed() -> void:
-	if not _mesh_container or not _mesh_container.is_inside_tree(): return
-	_mesh_container.rotation_degrees = Vector3(0.0, 0.0, twist_degrees)
-
-
-func _update_spring() -> void:
-	if not is_inside_tree() \
-		or not target_node or not target_node.is_inside_tree() \
-		or not anchor_node or not anchor_node.is_inside_tree() :
+func _update_geometry() -> void:
+	if not is_node_ready():
 		return
 
-	var origin := anchor_node.global_position
-	var target := target_node.global_position
+	_handle_anchor_target_changes()
+	_update_bounding_box()
+	_update_spring_geometry_for_editor()
 
-	if origin != _origin_last_position or target != _target_last_position:
-		_origin_last_position = origin
-		_target_last_position = target
+	_geometry_state_dirty = false
 
-		var distance := origin.distance_to(target)
-		if not origin.is_equal_approx(target):
-			var z_axis := origin.direction_to(target)
-			if absf(_current_up_axis.dot(z_axis)) > 0.99:
-				_current_up_axis = Vector3.RIGHT if absf(z_axis.y) > 0.9 else Vector3.UP
+func _update_spring_geometry_for_editor() -> void:
+	if not Engine.is_editor_hint() or mesh is not CylinderMesh:
+		return
+	var cylinder: CylinderMesh = mesh
+	var radius := coil_radius + wire_radius
+	var height := _current_length + wire_radius + wire_radius
+	if not is_equal_approx(cylinder.top_radius, radius): cylinder.top_radius = radius
+	if not is_equal_approx(cylinder.bottom_radius, radius): cylinder.bottom_radius = radius
+	if not is_equal_approx(cylinder.height, height): cylinder.height = height
 
-			var x_axis := _current_up_axis.cross(z_axis).normalized()
-			var y_axis := z_axis.cross(x_axis).normalized()
-			_current_up_axis = y_axis
-			global_transform = Transform3D(Basis(x_axis, y_axis, z_axis).orthonormalized(), (origin + target) * 0.5)
 
-		current_length = distance
+func _handle_anchor_target_changes() -> void:
+	var anchor_alive := anchor_node and anchor_node.is_node_ready()
+	var target_alive := target_node and target_node.is_node_ready()
+	var origin := anchor_node.global_position if anchor_alive else -Vector3.UP
+	var target := target_node.global_position if target_alive else Vector3.UP
+	_current_length = origin.distance_to(target) - offset
+	if not origin.is_equal_approx(target):
+		var y_axis := origin.direction_to(target)
+		if absf(_current_up_axis.dot(y_axis)) > 0.99:
+			_current_up_axis = Vector3.FORWARD if absf(y_axis.y) > 0.9 else Vector3.UP
+		var x_axis := _current_up_axis.cross(y_axis).normalized()
+		var z_axis := x_axis.cross(y_axis).normalized()
+		var tx := Basis(x_axis, y_axis, z_axis).rotated(y_axis, deg_to_rad(twist_degrees))
+		global_transform = Transform3D(tx.orthonormalized(), (target + origin) * 0.5)
+		_current_up_axis = y_axis
+
+
+func _update_bounding_box() -> void:
+	var radius := coil_radius + wire_radius
+	var height := _current_length + wire_radius * 2.0
+	custom_aabb.position = Vector3(-radius, -height * 0.5, -radius)
+	custom_aabb.size = Vector3(radius * 2.0, height, radius * 2.0)
+
+
+func _align_handles() -> void:
+	if anchor_node and anchor_node.is_node_ready():
+		anchor_node.look_at(global_position)
+	if target_node and target_node.is_node_ready():
+		target_node.look_at(global_position)
 
 
 func _update_shader() -> void:
-	if not is_inside_tree() or not _spring_mesh_instance:
+	if not is_node_ready():
 		return
 
-	var radius := coil_radius + wire_radius
-	var coil_diameter := radius * 2.0
-	var height := current_length + wire_radius * 2.0
-	_spring_mesh_instance.custom_aabb.position = Vector3(-radius, -height * 0.5, -radius)
-	_spring_mesh_instance.custom_aabb.size = Vector3(coil_diameter, height, coil_diameter)
-
-	var cylinder := _spring_mesh_instance.mesh as CylinderMesh
+	var cylinder := mesh as CylinderMesh
 	if cylinder:
-		cylinder.rings = rings
-		cylinder.radial_segments = radial_segments
+		if not is_equal_approx(cylinder.rings, rings):
+			cylinder.rings = rings
+		if not is_equal_approx(cylinder.radial_segments, radial_segments):
+			cylinder.radial_segments = radial_segments
 
-	if not _shader_material:
-		return
+		var active_material := cylinder.material as ShaderMaterial
+		if not active_material:
+			return
 
-	_shader_material.set_shader_parameter(&"albedo", albedo)
-	_shader_material.set_shader_parameter(&"metallic", metallic)
-	_shader_material.set_shader_parameter(&"specular", specular)
-	_shader_material.set_shader_parameter(&"roughness", roughness)
-	_shader_material.set_shader_parameter(&"wire_radius", wire_radius)
-	_shader_material.set_shader_parameter(&"coil_radius", coil_radius)
-	_shader_material.set_shader_parameter(&"total_coils", total_coils)
-	_shader_material.set_shader_parameter(&"current_length", current_length)
+		active_material.set_shader_parameter(&"wire_radius", wire_radius)
+		active_material.set_shader_parameter(&"coil_radius", coil_radius)
+		active_material.set_shader_parameter(&"total_coils", total_coils)
+		active_material.set_shader_parameter(&"height_normalize", 1.0 / cylinder.height)
+		active_material.set_shader_parameter(&"current_length", _current_length)
+		active_material.set_shader_parameter(&"albedo", albedo)
+		active_material.set_shader_parameter(&"metallic", metallic)
+		active_material.set_shader_parameter(&"specular", specular)
+		active_material.set_shader_parameter(&"roughness", roughness)
 
-	if Engine.is_editor_hint():
-		mesh = _cylinder_maker.generate(coil_radius + wire_radius * 0.5, current_length)
-		set_surface_override_material(0, _cached_tube_material)
-	else:
-		mesh = null
+	_shader_state_dirty = false
+
+func _set(property: StringName, value) -> bool:
+	if property == &"mesh":
+		if value != mesh:
+			_geometry_state_dirty = true
+			_shader_state_dirty = true
+			#if value and value is CylinderMesh and value.changed.is_connected(_update_shader):
+				#value.changed.disconnect(_update_shader)
+			mesh = value
+			_on_mesh_resource_changed(mesh)
+			return true
+	return false
 
 
-static func path_to(script: Variant, file: String) -> String:
-	return script.resource_path.get_base_dir().path_join(file)
+func _on_mesh_resource_changed(the_mesh: Mesh) -> void:
+	if the_mesh and the_mesh is CylinderMesh:
+		var material := ShaderMaterial.new()
+		material.shader = SHADER
+		(the_mesh as CylinderMesh).material = material
--- stress/stress_test.gd
+++ stress/stress_test.gd
@@ -6,8 +6,10 @@ extends Node3D
 @onready var camera_control: Node3D = $CameraControl
 @onready var camera_3d: Camera3D = $CameraControl/Camera3D
 
-var _time: float = 0.0
+var _time := 0.0
 var _thread: Thread
+var _running := true
+
 
 func _ready() -> void:
 	_boot_strap.call_deferred()
@@ -20,28 +22,39 @@ func _process(delta: float) -> void:
 	camera_control.rotate_y(delta * 0.05)
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"ui_cancel"):
+		_running = false
+		_wait_to_finish()
+		get_tree().quit()
+
+
 func _boot_strap() -> void:
 	_thread = Thread.new()
 	_thread.start(_generate_mucho_springs, Thread.PRIORITY_LOW)
 
 
-func _exit_tree():
+func _wait_to_finish() -> void:
 	if _thread && _thread.is_started():
 		_thread.wait_to_finish()
+
+
+func _exit_tree():
+	_wait_to_finish()
 
 
 func _generate_mucho_springs() -> void:
 	var size := 50
 	for z: int in range(-size, size, 2):
 		for x: int in range(-size, size, 2):
+			if !_running:
+				return
 			var vx := randf_range(-1.0, 1.0)
 			var vz := randf_range(-1.0, 1.0)
 			var jitter := Vector3(vx, 0, vz).normalized() * 0.5
 			var pos := Vector3(x, 0.0, z) + jitter
 			if pos.length() < size:
 				var thing: Node3D = spring_scene.instantiate()
-				thing.visible = false
 				thing.position = pos
 				springs.call_deferred(&"add_child", thing)
 				OS.delay_usec(50)
-				thing.visible = true
